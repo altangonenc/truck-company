3521b8e79caa62ff284ac1ac128f20921348e1a5
fiseqqq <altangonenc10@gmail.com>
2023-05-27 21:22:23 +0300

    Initial commit

---
+++ src/main/java/com/fiseq/truckcompany/TruckCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.fiseq.truckcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiseq/truckcompany/TruckCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiseq.truckcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
