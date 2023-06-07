68b1567a29b35d2ca7d40e4c032dca818314c632
fiseqqq <altangonenc10@gmail.com>
2023-06-07 14:01:42 +0300

    hot fix for a test

--- src/test/java/com/fiseq/truckcompany/TruckCompanyApplicationTests.java
+++ src/test/java/com/fiseq/truckcompany/TruckCompanyApplicationTests.java
@@ -1,9 +1,11 @@
 package com.fiseq.truckcompany;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class TruckCompanyApplicationTests {
 
 	@Test
