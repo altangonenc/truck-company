b8080ee8ec0835385d519df1952229fcaadf44c9
fiseqqq <altangonenc10@gmail.com>
2023-11-05 23:53:03 +0300

    update jar functionality added to gui script

--- scripts/desktop-starter/gui.py
+++ scripts/desktop-starter/gui.py
@@ -1,10 +1,30 @@
 import tkinter as tk
 import os
+import time
+from tkinter import messagebox
 
 def run_application():
     os.system(r'C:\Users\altan\Desktop\truck-company-all\truck-company\scripts\desktop-starter\dist\desktop-start.exe')
 
+def update_jar():
+    try:
+        #mvn clean
+        os.system('mvn clean')
+        #mvn install 
+        os.system('mvn install')
+
+        messagebox.showinfo("Succesfull", "mvn clean and mvn install operations executed successfully.")
+        
+    except Exception as e: 
+        print (f"Exception occured in maven operations. Exception is: {e}")
+    
+
 root = tk.Tk()
-button = tk.Button(root, text="Start Application", command=run_application)
-button.pack()
+
+start_button = tk.Button(root, text="Start Application", command=run_application)
+start_button.pack(pady=10)
+
+update_button = tk.Button(root, text="Update jar", command=update_jar)
+update_button.pack(pady=10)
+
 root.mainloop()
